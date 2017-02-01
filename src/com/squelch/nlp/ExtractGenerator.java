06360d2146ff6e1ba7d16ad1201649713b34da32
zeeiyerWork <zeeiyeredu@gmail.com>
2017-01-31 16:43:44 -0800

    Various changes to support QueryAnalyzer (NER, Sentiment, POS) and ExtractGenerator (auto summarizer)
    Various changes to support QueryAnalyzer (NER, Sentiment, POS) and
    ExtractGenerator (auto summarizer)

--- src/com/squelch/nlp/ExtractGenerator.java
+++ src/com/squelch/nlp/ExtractGenerator.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
@@ -15,47 +14,57 @@ import org.slf4j.LoggerFactory;
 
 import static java.nio.file.Files.readAllBytes;
 import static java.nio.file.Paths.get;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
 
 public class ExtractGenerator {
     private static final Logger LOG = LoggerFactory.getLogger(ExtractGenerator.class);
+    private StanfordCoreNLP pipeline = null;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	   if (args.length < 1) {
-		   System.out.println("Filename required");
+	   if (args.length < 2) {
+		   LOG.error("Usage: ExtractGenerator [InputFilename:String] [useAuthenticML:boolean]");
+	   } else {
+		   String fileName = args[0];
+		   boolean useAuthenticML = (args[1].equalsIgnoreCase("false")) ? false : true;
+		   String extractedSummary = (new ExtractGenerator()).summarize(fileName, useAuthenticML);
+		   LOG.info("EXTRACTED SUMMARY For file: " + fileName + ": " + System.getProperty("line.separator") + extractedSummary);
 	   }
-	   String fileName = args[0];
-	   String extractedSummary = (new ExtractGenerator()).summarize(fileName);
-	   LOG.info("For file: " + fileName + ". The Extracted summary: " + extractedSummary);
 	}
 
-	public String summarize(String filename) {
-		final int UPPER_THRESHOLD = 10;
+	public String summarize(String fileName) {
+		return summarize(fileName,false);
+	}
+	
+	public String summarize(String filename, boolean useAuthenticML) {
 		// using StanfordNLP get Sentences in Text Document.
-		ArrayList<String> allSentences = getAllSentences(filename);
+		List<CoreMap> allSentences = getAllSentences(filename);
+		LOG.info("SENTENCE COUNT for file: " + filename + " : " + allSentences.size());
 		
-		// assign a score to each Sentence - (fake implementation for now)
-        ConcurrentHashMap<String, Integer> scoredSentences = assignScoreToSentences(allSentences);
+		// assign a score to each Sentence - (fake/real implementation based on boolean)
+        ConcurrentHashMap<String, Integer> scoredSentences = assignScoreToSentences(allSentences, useAuthenticML);
         
         // sort this Map by value (i.e. score)
         LinkedHashMap<String, Integer> sortedSentences = (LinkedHashMap<String, Integer>) sortSentencesByValue(scoredSentences);
      
         // get the top few, based on an upper threshold.
-        ArrayList<String> topSentences = filterTopSentences(sortedSentences, UPPER_THRESHOLD);
+        ArrayList<String> topSentences = filterTopSentences(sortedSentences);
       
-        // classify each of the top as indicative of the document - (fake implementation for now)
-        ArrayList<String> indicativeSentences = classifyAsIndicative(topSentences);
+        // classify each of the top as indicative of the document - (fake/real implementation based on boolean)
+        ArrayList<String> indicativeSentences = classifyAsIndicative(topSentences, useAuthenticML);
 
         // concatenate the result to provide a summary.
         return generateExtractionSummary(indicativeSentences);
     }
 	
-	private ArrayList<String> getAllSentences(String filename) {
-		 ArrayList<String> allSentences = new ArrayList<String>();
+	private List<CoreMap> getAllSentences(String filename) {
+		 List<CoreMap> allSentences = new ArrayList<CoreMap>();
 		 // read the file into the text variable.
 		 String text = "";
 		 try {
@@ -71,10 +80,9 @@ public class ExtractGenerator {
 		 Annotation document = new Annotation(text);
 
 		 // creates a StanfordCoreNLP object, 
-		 Properties props = new Properties();
-		 props.put("annotators", "tokenize, ssplit");
-		 StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-		 
+		 String[] neededAnnotations = {"tokenize, ssplit"};			
+		 pipeline = new StanfordCoreNLP(new QueryAnalyzer().getNeededProps(neededAnnotations, false,null));  
+		
 		 // run all Annotators on this text
 		 pipeline.annotate(document);
 
@@ -82,33 +90,85 @@ public class ExtractGenerator {
 		 // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
 		 List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 
-		 for(CoreMap sentence: sentences) {
-		   allSentences.add(sentence.toString());
-		 }
-		 return allSentences;
+		 return sentences;
 	 }
 	
-	private ConcurrentHashMap<String, Integer> assignScoreToSentences(ArrayList<String> allSentences) {
+	private ConcurrentHashMap<String, Integer> assignScoreToSentences(List<CoreMap> allSentences, boolean useAuthenticML) {
 		ConcurrentHashMap<String, Integer> scoredSentences = new ConcurrentHashMap<String, Integer>();
-		for (String sentence: allSentences) {
-			int theScore = getScore(sentence);
+	
+		// variables for score calculations
+		int TOP_FEW = 5;
+		int BOTTOM_FEW = 5;
+		int POSITION_SCORE = 10;
+		int KEY_PHRASE_SCORE = 10;
+		int TITLE_TOKEN_SCORE = 10;
+		int BASE_SCORE_MINWORDS_SENTENCE = 20;
+		int MINWORDS_SENTENCE = 3;		
+		List<String> titleTokens = new ArrayList<String>();
+		List<String> sentenceTokens = new ArrayList<String>();
+		String[] keyPhraseList = {"Introduction", "In summary", "To summarize", "In conclusion", "To Conclude", "Document describes"};
+		
+		Integer sentencePosition = 0;
+		Integer theScore = 0;
+
+  		for (CoreMap sentenceCoreMap : allSentences) {
+			String sentence = sentenceCoreMap.toString();
+			if (! useAuthenticML) {
+				// For this option - just return a random score.
+				try {
+					theScore = new Integer((int) Math.round(100*Math.random()));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			else {
+				// Get Features and score appropriately: NAIVE IMPLEMENTATION TO ILLUSTRATE CONCEPT.
+				// -- Length: MinWords
+				// -- Position: Is this sentence in the top few or bottom few
+				// -- Content: Does the sentence contain key phrases
+				// -- Content: Does the sentence contain any words from the Title.
+				// TBD: Does the sentence contain any words from the top 20% Frequent Terms.			
+				sentencePosition++;		
+				for (CoreLabel token : sentenceCoreMap.get(TokensAnnotation.class)) {
+					String word       = token.get(TextAnnotation.class);
+					Boolean added     = (sentencePosition == 1) ? titleTokens.add(word) : sentenceTokens.add(word);
+				}
+				// scoring
+				if (sentencePosition == 1) {
+					theScore = Integer.MAX_VALUE; // Always include 1st Sentence = Title.
+				} else {
+					theScore = 0;
+					// Does sentence have at least MINWORDS
+					if (sentenceTokens.size() > MINWORDS_SENTENCE) {
+						theScore += BASE_SCORE_MINWORDS_SENTENCE;
+					}
+					// Is sentence at Top or Bottom of Document
+					if ((sentencePosition < TOP_FEW) || (sentencePosition > allSentences.size() - BOTTOM_FEW)) {
+						theScore += POSITION_SCORE;
+					}
+					// Does sentence have keyPhrases
+					for (String keyPhrase : keyPhraseList) {
+						if (sentence.toLowerCase().contains(keyPhrase.toLowerCase())) {
+							theScore += KEY_PHRASE_SCORE;
+						}
+					}
+					// Does sentence have words in common with the title.
+					for (String titleToken: titleTokens) {
+						for (String sentenceToken : sentenceTokens) {
+							if (titleToken.toLowerCase().equals(sentenceToken.toLowerCase())) {
+								theScore += TITLE_TOKEN_SCORE;
+								break;
+							}
+						}
+					}
+				}					
+			}
 			scoredSentences.put(sentence, theScore);
-			LOG.info("SCORE/SENTENCE ::: " + theScore + " ===> " + sentence.toString());
+			LOG.trace("SCORE/SENTENCE ::: " + theScore + " ===> " + sentence.toString());
 		}
 		return scoredSentences;
 	}
 	
-	private Integer getScore(String anySentence) {
-		// Get features for this sentence and then score based on the features.
-		// For now , just return a random score.
-		Integer theScore = 0;
-		try {
-			theScore = new Integer((int) Math.round(100*Math.random()));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return theScore;
-	}
 	
 	public static <K, V extends Comparable<? super V>> Map<K, V> sortSentencesByValue(Map<K, V> allSentences) {			 
 		Map<K, V> sortedResult = new LinkedHashMap<>();
@@ -118,11 +178,12 @@ public class ExtractGenerator {
 		return sortedResult;
 	}
 		 
-	private ArrayList<String> filterTopSentences(LinkedHashMap<String, Integer> sortedSentences, int maxThreshold) {
+	private ArrayList<String> filterTopSentences(LinkedHashMap<String, Integer> sortedSentences) {
 		ArrayList<String> topChosenSentences = new ArrayList<String>();
+	    final int UPPER_THRESHOLD_NUMSENTENCES = 10;
 		int count = 0;
 		for (String key : sortedSentences.keySet()) {
-			if (count < maxThreshold) {
+			if (count < UPPER_THRESHOLD_NUMSENTENCES) {
 				topChosenSentences.add(key);
 				LOG.info("TOP-SCORE/TOP-SENTENCE ::: " + sortedSentences.get(key) + " ===> " + key);
 				count++;
@@ -133,20 +194,28 @@ public class ExtractGenerator {
 		return topChosenSentences;
 	}
 	 
-	 private ArrayList<String> classifyAsIndicative(List<String> topSentences) {
+	 private ArrayList<String> classifyAsIndicative(List<String> topSentences, boolean useAuthenticML) {
 		 ArrayList<String> indicativeSentences = new ArrayList<String>();
-		 Random r = new Random();
-		 for (String s : topSentences) {
-			 if (r.nextBoolean()) {
-				 indicativeSentences.add(s);
+		 // TBD - Fix This
+		 if (!useAuthenticML || useAuthenticML) {
+			 // Just Randomly classify it as indicative or not
+			 Random r = new Random();
+			 int count = 0;
+			 for (String s : topSentences) {
+				 count ++;
+				 if ((r.nextBoolean()) || (count == 1)) {
+					 indicativeSentences.add(s);
+				 }
 			 }
+		 } else {
+			 // Use Real ML to arrive at the answer.
 		 }
 		 return indicativeSentences;
 	 }
 	 	
 	 private String generateExtractionSummary(ArrayList<String> indicativeSentences) {
 		 String summary = "";
-		 final String SEPARATOR = "+++++";
+		 final String SEPARATOR = System.getProperty("line.separator") + "........" ;
 		 for (String indicativeSentence : indicativeSentences) {
 	                summary += indicativeSentence + SEPARATOR;
 	        }
--- src/com/squelch/nlp/QueryAnalyzer.java
+++ src/com/squelch/nlp/QueryAnalyzer.java
@@ -13,6 +13,9 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
@@ -26,19 +29,47 @@ import edu.stanford.nlp.util.CoreMap;
 
 public class QueryAnalyzer {
     private static final Logger LOG = LoggerFactory.getLogger(QueryAnalyzer.class);
+    private static final String DEFAULT_DICTIONARY_NAME = "squelchDictionary.txt";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Send a file - which has all the input tickets that need to be analyzed.
 	   String fileName = "";
-	   if (args.length < 1) {
-		  LOG.error("Filename required");
+	   // Indicate if custom dictionary is needed 
+	   boolean useCustomDictionary = true;
+	   // DictionaryName 
+	   String dictionaryName = "";
+	   
+	   switch(args.length) {
+	   case 0:
+		   LOG.error("Usage: QueryAnalyzer [inputFileName], <useCustomDictionary>, <dictionaryFileName>");
+		   break;
+	   case 1:
+		   fileName = args[0];
+		   break;
+	   case 2:
+		   fileName = args[0];
+		   useCustomDictionary = (args[1].equalsIgnoreCase("false")) ? false : true;
+		   dictionaryName = DEFAULT_DICTIONARY_NAME;
+		   break;
+	   default:
+		   fileName = args[0];
+		   useCustomDictionary = (args[1].equalsIgnoreCase("false")) ? false : true;
+		   dictionaryName = args[2];
 	   }
-	   fileName = args[0];
-	   (new QueryAnalyzer()).analyze(fileName);
+	 
+	   (new QueryAnalyzer()).analyze(fileName, useCustomDictionary, dictionaryName);
 	}
 
+	
 	@Test
 	public void analyze(String fileName) {
+		analyze (fileName, false, null);
+	}
+	public void analyze(String fileName, boolean useCustomDictionary) {
+		analyze (fileName, useCustomDictionary, QueryAnalyzer.DEFAULT_DICTIONARY_NAME);
+	}
+	
+	public void analyze (String fileName, boolean useCustomDictionary, String dictionaryName) {	
 	    LOG.info("Stanford CoreNLP: Starting to analyze contents of file: " + fileName);
 	    Stream<String> ticketTitleStream = null;
 	    try {
@@ -47,52 +78,88 @@ public class QueryAnalyzer {
 	    	e.printStackTrace();
 	    }
 	    
+	    LOG.info("Get Content of Each Ticket");
 	    ArrayList<String> ticketInputs = new ArrayList<String>();
 	    Iterator<String> iTicket = ticketTitleStream.iterator();
 	    while (iTicket.hasNext()) {
 	    	ticketInputs.add(iTicket.next());
 	    }
 	    
-	    Properties props = new Properties();
-	    props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, sentiment");
-	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-		 
+	    String[] neededAnnotations = {"tokenize, ssplit, pos, lemma, ner, parse, sentiment"};			
+	    StanfordCoreNLP pipeline = new StanfordCoreNLP(getNeededProps(neededAnnotations, useCustomDictionary, dictionaryName));  
 	    for (String s : ticketInputs) {
-	    	// run all annotators on the passed-in text
-    		LOG.info("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-    		LOG.info("input Ticket Text:" + "\t" + s);
-    		Annotation document = new Annotation(s);
-	    	pipeline.annotate(document);
-
-	    	// these are all the sentences in this document
-	    	// a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-	    	List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-		      	    	
-	    	for (CoreMap sentence : sentences) {
-	    		// get Sentiment for the sentence.
-	    		String sentenceSentiment = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
-	    		LOG.info(sentenceSentiment + ":\t" + sentence.toString());
-
-		        // traversing the words in the current sentence, "O" is a sensible default to initialize
-		        // tokens to since we're not interested in unclassified / unknown things..
-		
-		        for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-			          String word       = token.get(TextAnnotation.class);
-			          String nerToken   = token.get(NamedEntityTagAnnotation.class);
-			          String posToken   = token.get(PartOfSpeechAnnotation.class);
-			          // One can get a lot more.
-			          // String lemmaToken = token.get(LemmaAnnotation.class);
-
-			          if (! nerToken.equals("O")) {
-			        	  LOG.info(word + ":\t" + nerToken  + ":\t" + posToken);
-			        	  // persist in MongoDB?
-			          }
-		        }
-	    	}
-		      
+	    	analyze(s, pipeline);
 	    }
 	}
 	
+	public Properties getNeededProps (String[] neededAnnotations, boolean useCustomDictionary, String dictionaryFileName) {
+	    Properties props = new Properties();
+		String theNeededAnnotations = "";
+		for (String theAnnotation : neededAnnotations) {
+			theNeededAnnotations += theAnnotation + ", ";
+		}
+		theNeededAnnotations = theNeededAnnotations.substring(0, theNeededAnnotations.length()-2);
+	    theNeededAnnotations += (useCustomDictionary) ? ", regexner"  : "";
+	    LOG.info ("Annotations in play: " + theNeededAnnotations);		
+	    props.put("annotators", theNeededAnnotations);
+	    if (useCustomDictionary) {
+		      props.put("regexner.mapping", dictionaryFileName);
+	    }
+	    return props;
+	}
+
+	
+	public void analyze (String s, StanfordCoreNLP pipeline) {
+		// run all annotators on the passed-in text
+  		LOG.info("=========================================================");
+  		LOG.info("input Ticket Text:" + "\t" + s);
+  		Annotation document = new Annotation(s);
+  		pipeline.annotate(document);
+
+  		// these are all the sentences in this document
+  		// a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+  		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+		      	    	
+  		
+  		for (CoreMap sentence : sentences) {
+  			// get SENTIMENT for the sentence.
+  			String sentenceSentiment = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
+  			LOG.info(sentenceSentiment + ":\t" + sentence.toString());
+  			
+  			// prepare Data Structures for POS and NER.
+  			
+  			// for POS
+  			ListMultimap<String, String> posMultiMap = ArrayListMultimap.create();
+
+  			// for NER:  not interested in unclassified / unknown things..
+  			String nerPreviousToken = "";
+  			String nerPhrase = "";
+  			
+  			for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+  				String word       = token.get(TextAnnotation.class);
+  				String posToken   = token.get(PartOfSpeechAnnotation.class);
+  				String nerToken   = token.get(NamedEntityTagAnnotation.class);
+  				
+  				// NER - process token appropriately., Only care about POS for words recognized by NER? 
+  				if (! nerToken.equals("O")) {
+  					// POS - process token into the DataStructure.
+  	  				posMultiMap.put(posToken, word);
+  					nerPhrase = (nerPreviousToken.equalsIgnoreCase(nerToken)) ?  nerPhrase + " " + word : word;
+  					nerPreviousToken = nerToken;
+  				} else {
+  					if ((nerPhrase.length() > 0)) {
+  						LOG.info (nerPreviousToken + " : " + nerPhrase);
+  						nerPhrase = "";
+  						nerPreviousToken = "";
+  					}
+  				}
+  			}	
+  			for (String eachPosToken : posMultiMap.keySet()) {
+  				LOG.info ("Part of Speech: " + eachPosToken + " - " + posMultiMap.get(eachPosToken));
+  			}
+  	  		LOG.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+  		}      
+	  }
 }
 	
 
